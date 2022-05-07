d688fc6dd5dd2afe3d6b3cf8d9e720586fd4f22a
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-05-07 12:12:55 -0300

    feat(footer): add footer component
    This commmit add the footer component to the page.
    Also, add the subtitle component.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,23 +5,30 @@ import Title from '../../components/Title';
 import Container from '../../components/Container';
 import Main from '../../components/Main';
 import User from '../../components/User';
+import Footer from '../../components/Footer';
+import Subtitle from '../../components/Subtitle';
 
-function Home({ title }) {
+function Home({ title, subtitle }) {
   return (
     <Container>
       <Header>
-        <Title>{ title }</Title>
+        <Title title={title} />
       </Header>
       <Main>
+        <Subtitle subtitle={subtitle} />
         <User />
         <User />
       </Main>
+      <Footer>
+        <Subtitle subtitle={subtitle} />
+      </Footer>
     </Container>
   );
 }
 
 Home.propTypes = {
   title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
 };
 
 export default Home;
