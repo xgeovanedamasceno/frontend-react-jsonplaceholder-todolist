2875d08944cde73a2bafbe5d22516f77f26ba774
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-05-07 15:07:46 -0300

    fix: fix* typechecking error
    This commit fix* typechecking error by removing
    proptypes check.

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,14 +1,10 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
-import PropTypes from 'prop-types';
 
 function Main({ children }) {
   return (
     <main>{ children }</main>
   );
 }
-
-Main.propTypes = {
-  children: PropTypes.arrayOf(PropTypes.element).isRequired,
-};
 
 export default Main;
