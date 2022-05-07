aae63dba6a10e581be0a4e188a6526f4d1ce412e
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-05-07 13:03:21 -0300

    refactor: add typechecking props
    This commit import proptypes library to check the type of
    the component's prop.

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Title() {
+function Title({ title }) {
   return (
-    <h1>React Json Placeholder TodoList</h1>
+    <h1>{ title }</h1>
   );
 }
+
+Title.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 export default Title;
