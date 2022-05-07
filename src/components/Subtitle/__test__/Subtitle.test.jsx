9c6b9fd9b1a4ceb6749a69f1779f04e2d2828d0b
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-05-07 06:11:38 -0300

    style: add and applies eslint rules
    This commit add and applies eslint rules:
    'React' must be in scope when using JSX
    JSX not allowed in files with extension '.js'

---
+++ src/components/Subtitle/__test__/Subtitle.test.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import Subtitle from '..';
+
+describe('Subtitle', () => {
+  beforeEach(() => {
+    render(<Subtitle />);
+  });
+
+  it('should render an h2 element', () => {
+    const h2El = screen.getByRole('heading', { level: 2 });
+
+    expect(h2El).toBeInTheDocument();
+  });
+});
