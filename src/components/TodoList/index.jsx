91e2a9d298e69142dd48ad621877011f7e1886a0
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-05-07 18:56:29 -0300

    refactor: remove typechecking props

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,14 +1,10 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
-import PropTypes from 'prop-types';
 
 function TodoList({ children }) {
   return (
     <ul>{ children }</ul>
   );
 }
-
-TodoList.propTypes = {
-  children: PropTypes.node.isRequired,
-};
 
 export default TodoList;
