ca8fdd877bd9e46702a550d93ab196c3809a12c7
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-05-07 17:05:40 -0300

    refactor: update the names of functions and variables
    This commit update the names of functions and variables due to
    the update of the TodoList component.

--- src/pages/UserProfile/index.jsx
+++ src/pages/UserProfile/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Header from '../../components/Header';
 import Title from '../../components/Title';
@@ -6,8 +7,52 @@ import Container from '../../components/Container';
 import Main from '../../components/Main';
 import Footer from '../../components/Footer';
 import Subtitle from '../../components/Subtitle';
+import User from '../../components/User';
+import TodoList from '../../components/TodoList';
 
 function UserProfile({ title, subtitle }) {
+  const [user, setUser] = useState(null);
+  const [todoList, setTodoList] = useState([]);
+
+  const params = useParams();
+
+  function fetchUser() {
+    fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
+      .then((response) => response.json())
+      .then((data) => setUser(data));
+  }
+
+  function fetchTodoList() {
+    fetch(`https://jsonplaceholder.typicode.com/todos?userid=${params.id}`)
+      .then((response) => response.json())
+      .then((data) => setTodoList(data));
+  }
+
+  useEffect(() => {
+    fetchUser();
+    fetchTodoList();
+  }, []);
+
+  function renderUser() {
+    return (
+      <User>
+        <h3>{user?.name}</h3>
+        <p>{user?.email}</p>
+      </User>
+    );
+  }
+
+  function renderTodoList() {
+    return (
+      todoList?.map((itemList, index) => (
+        <li key={`${itemList?.id}${itemList.id + index}`}>
+          <p>{itemList.title}</p>
+          <p>{`${itemList.completed}`}</p>
+        </li>
+      ))
+    );
+  }
+
   return (
     <Container>
       <Header>
@@ -15,6 +60,10 @@ function UserProfile({ title, subtitle }) {
       </Header>
       <Main>
         <Subtitle subtitle={subtitle} />
+        { renderUser() }
+        <TodoList>
+          { renderTodoList() }
+        </TodoList>
       </Main>
       <Footer>
         <Subtitle subtitle={subtitle} />
