f3a672e5b63d789299ca27b7b924e4083dd48ee1
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-05-08 14:40:38 -0300

    refactor: add the dumb components to app.jsx file
    This commit add the components to app.jsx file to
    improve the code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,40 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import UserProfile from './pages/UserProfile';
+import Container from './components/Container';
+import Header from './components/Header';
+import Title from './components/Title';
+import Footer from './components/Footer';
+import Subtitle from './components/Subtitle';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Home title="TODO USERS APP" subtitle="A React Application" />
+      <Container>
+        <Header>
+          <Title title="Header Title" />
+        </Header>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home subtitle="Users Page" />
         }
-        />
-        <Route
-          path="/user/:id"
-          element={
-            <UserProfile title="Todo User APP" subtitle="User Profile - Todolist" />
+          />
+          <Route
+            path="/user/:id"
+            element={
+              <UserProfile title="Todo User APP" subtitle="User Profile - Todolist" />
           }
-        />
-      </Routes>
+          />
+        </Routes>
+        <Footer>
+          <Subtitle subtitle="Footer Subtitle App" />
+        </Footer>
+      </Container>
     </BrowserRouter>
   );
 }
