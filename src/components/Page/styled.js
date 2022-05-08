73c437ee5965d54e5936aef3a61f5540d273d099
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-05-08 18:05:36 -0300

    feat(design-ui): add style to the buttons and listItems

---
+++ src/components/Page/styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const MainStyle = styled.main`
+form {
+    border: 1px solid #e0e0e0;
+    width: 700px;
+    padding: 20px;
+}
+
+input {
+    padding: 5px;
+    margin-right: 20px;
+    width: 590px;
+}
+
+button {
+    padding: 5px 20px;
+}
+`;
+
+export default MainStyle;
--- src/components/TodoList/styled.js
+++ src/components/TodoList/styled.js
@@ -2,9 +2,27 @@ import styled from 'styled-components';
 
 const TodoListStyle = styled.ul`
 border: 1px solid #e0e0e0;
-width: 700px;
+width: 800px;
 margin-top: 15px;
-padding: 40px
+padding: 30px;
+padding-top: 10px;
+
+#task-description {
+    
+}
+
+#buttons-sec {
+    margin-bottom: 20px;
+}
+
+button {
+    margin-right: 10px;
+}
+
+li {
+    border-bottom: 1px solid #E5E5E5;
+    margin-bottom: 40px;
+}
 `;
 
 export default TodoListStyle;
