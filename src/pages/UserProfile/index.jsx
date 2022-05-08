85c356791fcf9c304e4cc20d06f0444ce75e6f2f
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-05-08 14:53:46 -0300

    refactor: remove dumb components from userpage
    This commit moves dumb components from userpage to
    app.jsx file.

--- src/pages/UserProfile/index.jsx
+++ src/pages/UserProfile/index.jsx
@@ -1,16 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import Header from '../../components/Header';
-import Title from '../../components/Title';
-import Container from '../../components/Container';
 import Main from '../../components/Main';
-import Footer from '../../components/Footer';
 import Subtitle from '../../components/Subtitle';
 import User from '../../components/User';
 import TodoList from '../../components/TodoList';
 
-function UserProfile({ title, subtitle }) {
+function UserProfile({ subtitle }) {
   const [user, setUser] = useState({});
   const [todoList, setTodoList] = useState([]);
   const [todoItem, setTodoItem] = useState('');
@@ -156,27 +152,18 @@ function UserProfile({ title, subtitle }) {
   }
 
   return (
-    <Container>
-      <Header>
-        <Title title={title} />
-      </Header>
-      <Main>
-        <Subtitle subtitle={subtitle} />
-        { renderUser() }
-        <TodoList>
-          { renderInputForm() }
-          { renderTodoList() }
-        </TodoList>
-      </Main>
-      <Footer>
-        <Subtitle subtitle={subtitle} />
-      </Footer>
-    </Container>
+    <Main>
+      <Subtitle subtitle={subtitle} />
+      { renderUser() }
+      <TodoList>
+        { renderInputForm() }
+        { renderTodoList() }
+      </TodoList>
+    </Main>
   );
 }
 
 UserProfile.propTypes = {
-  title: PropTypes.string.isRequired,
   subtitle: PropTypes.string.isRequired,
 };
 
