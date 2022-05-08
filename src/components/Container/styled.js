60ffb0221ed3cf41e09ef311a918606d58a9ee4b
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-05-08 17:12:53 -0300

    feat(design-ui): add style to h1, h2, anchor and font

--- src/components/Container/styled.js
+++ src/components/Container/styled.js
@@ -7,6 +7,17 @@ flex-wrap: wrap;
 width: 1000px;
 margin: 0 auto;
 background-color: f7f7f7;
+
+h1, h2 {
+    color: #1C1D1F;
+}
+
+a {
+    text-decoration: none;
+    color: #1A1B1C;
+}
+
+font-family: monospace;
 `;
 
 export default ContainerStyle;
